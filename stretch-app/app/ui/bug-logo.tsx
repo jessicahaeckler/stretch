4637464ef961f6bab301f78b919b8cf407198030
jhaeckler167 <jhaeckler167@gmail.com>
2026-06-12 19:36:17 -0500

    Added pages to app, learning next js

---
+++ stretch-app/app/ui/bug-logo.tsx
@@ -0,0 +1,13 @@
+import { BugAntIcon } from "@heroicons/react/24/outline";
+import { inter } from "@/app/ui/fonts";
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${inter.className} flex flex-row items-center leading-none text-white`}
+    >
+      <BugAntIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Web</p>
+    </div>
+  );
+}
